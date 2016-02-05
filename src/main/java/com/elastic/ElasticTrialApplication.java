dac5c0e1953dba5bebbea2dd780e5526d6e81003
Chris James <chris.james@fusion-experience.com>
2016-02-05 11:04:48 +0000

    added basic spring boot elastic search data project

---
+++ src/main/java/com/elastic/ElasticTrialApplication.java
@@ -0,0 +1,12 @@
+package com.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticTrialApplication.class, args);
+	}
+}
